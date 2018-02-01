82b39721ed1312fcdd9c659b4fcc840678977a3f
RomainVanmarcke <romain.vanmarcke@isen-lille.fr>
2018-02-01 15:33:46 -0500

    Update and rename wholeArithmeticCrossover.m to localArithmeticCrossover.m

--- localArithmeticCrossover.m
+++ localArithmeticCrossover.m
@@ -1,4 +1,4 @@
-function children = wholeArithmeticCrossover(firstParent, secondParent)
+function children = localArithmeticCrossover(firstParent, secondParent)
 alpha = rand(1,length(firstParent));
 firstChild = alpha.*firstParent + (1-alpha).*secondParent;
 secondChild = alpha.*secondParent + (1-alpha).*firstParent;
