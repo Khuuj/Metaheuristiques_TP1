1b9719d1c33136675db4d2fcb2685f80ea931b47
Khuuj <ninotna95@hotmail.fr>
2018-02-01 17:05:56 -0500

    correction boundary mutation

--- boundaryMutation.m
+++ boundaryMutation.m
@@ -1,10 +1,10 @@
-function child = boundaryMutation(pm, child)
+function child = boundaryMutation(pm, child, upper, lower)
 for i=1:length(child)
         if rand<pm
             if rand>0.5
-                child(i)=1;
+                child(i)=upper;
             else
-                child(i)=0;
+                child(i)=lower;
             end
         end
 end
