b266a244b69d1d999170ce2cdcef939f18b0c222
Romain <romain.vanmarcke@isen.yncrea.fr>
2018-02-08 23:02:00 -0500

    Mutation fixed

--- Mutation/boundaryMutation.m
+++ Mutation/boundaryMutation.m
@@ -2,9 +2,9 @@ function child = boundaryMutation(child, pm, lower, upper, ~, ~, ~, ~, ~)
 for i=1:length(child)
         if rand<pm
             if rand>0.5
-                child(i)=upper;
+                child(i)=upper(i);
             else
-                child(i)=lower;
+                child(i)=lower(i);
             end
         end
 end
--- mutation.m
+++ mutation.m
@@ -1,6 +1,6 @@
 function children = mutation(mutationFunction, children, pm, lower, upper, b, g, Gmax, n, sigmaVector)
 	%FIXME : lower and upper are now vectors (check all mutations)
 	for i=1:length(children)
-	    children(i) = mutationFunction(children(i), pm, lower, upper, b, g, Gmax, n, sigmaVector);
+	    children(i,:) = mutationFunction(children(i,:), pm, lower, upper, b, g, Gmax, n, sigmaVector);
 	end
 end
