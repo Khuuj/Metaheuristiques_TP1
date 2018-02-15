ce9ce314eb9ce7b84a649f3b244f0def35bbcacf
Khuuj <ninotna95@hotmail.fr>
2018-02-15 00:13:08 -0500

    recorrection stochastic universal sampling

--- Selection/stochasticUniversalSampling.m
+++ Selection/stochasticUniversalSampling.m
@@ -2,10 +2,11 @@ function selected = stochasticUniversalSampling(scoresg, M, ~)
     r = rand;
     angle = 1/M;
     selected = zeros(M,1);
+    sumScore = sum(scoresg);
     for i=1:M
-        arrow = r*sum(scoresg)+(i-1)*angle;
-        if arrow>=1
-            arrow = arrow-1;
+        arrow = (r+(i-1)*angle)*sum(scoresg);
+        if arrow>=sum(scoresg)
+            arrow = arrow-sum(scoresg);
         end
         compteur = 0;
         while arrow >= 0
